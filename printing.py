9e69dca2d23822218db6e90bd977b905acc02a94
sfaat Husain <sfaathusainpintu@gmail.com>
2021-05-11 16:01:59 +0530

    rendom File

---
+++ printing.py
@@ -0,0 +1,13 @@
+# you="sfaat husain"
+# print("this is you " + you)
+# print("this is you {}".format(you))
+# print(f"this is you{you}")
+s1=input("string")
+s2=input("integer")
+s4=input("flote")
+s3=input("boolean")
+
+
+printing=f"this is a name{s1}. it is use for presenting \\ i wont yo {s2} \
+id i ghjk fgnnconnnvnvn{s3} trbbfghcjjjc{s4}"
+print(printing)
---
+++ rendumFile.py
@@ -0,0 +1,7 @@
+from multipalFileStringprint import first, secund, three, four
+import random
+
+if __name__ == "__main__":
+    m=random.choice([first, secund, three, four])
+    m.madlib()
+    
